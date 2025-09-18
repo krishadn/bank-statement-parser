513fad7d01f2a4aaf31ee02d180b08e3c8cc3d9b
krishadn <krizziasantillan@gmail.com>
2025-09-18 10:49:06 +0800

    defined the test cases for extracting text from pdf (empty and non-empty pdf)

--- src/test/java/kpes/finapp/service/BankStatementParserServiceTest.java
+++ src/test/java/kpes/finapp/service/BankStatementParserServiceTest.java
@@ -2,8 +2,12 @@ package kpes.finapp.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +15,36 @@ import static kpes.finapp.service.BankStatementParserService.extractText;
 
 public class BankStatementParserServiceTest {
 
-
+    // String paths to test files
+    String pdfPath = "C:\\Users\\KPES\\Desktop\\services\\sampleStatement.pdf";
+    String testFilePath = "C:\\Users\\KPES\\Desktop\\services\\testString.txt";
+    String emptyPdfPath = "C:\\Users\\KPES\\Desktop\\services\\empty.pdf";
+    
+  
     // extractText outputs correct String value for non empty pdf
     @Test
     void testNonEmptyPDF() {
+        Path pdfFile = Paths.get(pdfPath);
+        Path testFile = Paths.get(testFilePath);
+
+        try {
+            Stream<String> lines = Files.lines(testFile); 
+            String data = lines.collect(Collectors.joining("\n"));
+            lines.close();
+
+            assertEquals(data, extractText(pdfFile));
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     // extractText outputs correct String value for empty pdf
     @Test
     void testEmptyPDF() {
-
+        Path emptyPdf = Paths.get(emptyPdfPath);
+        assertEquals("", extractText(emptyPdf));
     }
 
     // extractText handles file does not exist
